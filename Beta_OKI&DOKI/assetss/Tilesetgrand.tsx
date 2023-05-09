df06adc41e10a6cc25130be458afc2a3901fbe9f
Léo GAUTHIER <118450880+Barnabuss@users.noreply.github.com>
2023-05-09 17:28:40 +0200

    Add files via upload

---
+++ Beta_OKI&DOKI/assetss/Tilesetgrand.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tilesetgrand" tilewidth="32" tileheight="32" tilecount="5162" columns="89">
+ <image source="Tilesetgrand.png" width="2876" height="1858"/>
+</tileset>
